c519cd06bacd9805550f9efadcc48558d0f46c49
Kaushik <kaushikchaubal@users.noreply.github.com>
2016-07-07 23:45:41 +0100

    Adding Github repo list

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // When?  To get a list of all your repos in bitbucket
-
 // What to do? Login >> Go to the homepage (https://bitbucket.org/) >> Open Dev Tools & Run:
 
-function printAllRepos() {
-  var repoList = [];
-
+function printAllBitBucketRepos() {
+  var bitBucketRepoList = [];
+  
 	$(".repo-list--repo-name").each(function() {
-    repoList.push($(this).text());
-	}); 
+	bitBucketRepoList.push($(this).text());
+}); 
 
-  return repoList;          
+  return bitBucketRepoList;          
 }
 
-console.log(repoList);
+console.log(bitBucketRepoList);
+
+// When?  To get a list of all your repos in github
+// What to do? Login >> Go to the homepage (For example: https://github.com/kaushikchaubal?tab=repositories) >> Open Dev Tools & Run:
+
+function printAllGithubRepos() {
+  var gitHubRepoList = [];
+  var codeTags = document.querySelectorAll('a[itemprop="name codeRepository"]')
+  for (i = 0; i < codeTags.length; i++) { 
+  	gitHubRepoList.push((codeTags[i]).innerHTML.trim());
+  }
+
+  return gitHubRepoList;          
+}
+
+console.log(gitHubRepoList);
